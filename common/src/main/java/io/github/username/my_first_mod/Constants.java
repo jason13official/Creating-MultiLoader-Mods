feffc1a2ecb48dd67d8936a6c644c7444f7e125d
jason13official <jason13official@gmail.com>
2025-07-04 11:59:48 -0400

    feat: refactor to tutorial namespace

---
+++ common/src/main/java/io/github/username/my_first_mod/Constants.java
@@ -0,0 +1,11 @@
+package io.github.username.my_first_mod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "my_first_mod";
+	public static final String MOD_NAME = "My First Mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/io/github/username/my_first_mod/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package io.github.username.my_first_mod.platform;
+
+import io.github.username.my_first_mod.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
