c4f2f88a6b312a7552726d52bcaad59c5c590528
jason13official <jason13official@gmail.com>
2025-07-04 14:51:42 -0400

    feat: MyFirstTabs docs

--- common/src/main/java/io/github/username/my_first_mod/core/registry/MyFirstTabs.java
+++ common/src/main/java/io/github/username/my_first_mod/core/registry/MyFirstTabs.java
@@ -8,8 +8,17 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
+/**
+ * Not everything can be registered in a simple fashion, sometimes mod loaders will change or patch portions of
+ * vanilla code to give modders a better development experience. However, we don't receive those benefits
+ * unless we can directly request them via platform helpers. <br />
+ */
 public class MyFirstTabs {
 
+    /**
+     * Here's a classic example; CreativeModeTabs are typically built with a {@link CreativeModeTab.Row} and an int value for their column index. <br />
+     * We don't want to arbitrarily define these values, so we use an additional platform helper during initialization to get the modified tab builder.
+     */
     public static final DeferredRegistryObject<CreativeModeTab> MY_FIRST_TAB =
             Services.PLATFORM.register(BuiltInRegistries.CREATIVE_MODE_TAB, "my_first_tab",
                     () -> Services.PLATFORM.tabBuilder()
